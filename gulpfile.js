8c8b21979a54e5e2879a8de0e968d18b259411df
sofiykaaa <sofiia.kindratyshyn.pm.2022@lpnu.ua>
2023-10-24 00:40:49 +0300

    changed the file content , added parts of the code , all tasks work

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+ const{series, src, dest, parallel, watch}= require('gulp');
+ const sass = require('gulp-sass')(require('sass'));
+ const concat = require("gulp-concat");
+ const uglify = require('gulp-uglify');
+ const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+
+const html_task = () => src('app/*.html')
+  .pipe(dest('dist'));
+ 
+const scss_task = () => src('app/scss/*.scss')
+ .pipe(concat('index.scss'))
+ .pipe(sass())
+ .pipe(cssnano())
+ .pipe(rename({suffix: '.min'}))
+ .pipe(dest('dist/css'));
+
+ const scripts_task = () => src('app/js/*.js')
+.pipe(concat('index.js'))
+.pipe(uglify())
+.pipe(rename({suffix: '.min'}))
+.pipe(dest('dist/js'));
+
+const watch_task = () => {
+    watch('app/*.html', parallel(html_task));
+    watch('app/scss/*.scss', parallel(scss_task));
+    watch('app/js/*.js', parallel(scripts_task));
+}
+
+exports.default=series(html_task, scss_task, scripts_task, watch_task);
+ 
