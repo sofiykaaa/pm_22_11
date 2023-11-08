7b524b28016612f557a62d2fe601be44efa45143
sofiykaaa <sofiia.kindratyshyn.pm.2022@lpnu.ua>
2023-11-08 23:24:24 +0200

    new

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
  const uglify = require('gulp-uglify');
  const rename = require('gulp-rename');
 const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
 
 const html_task = () => src('app/*.html')
   .pipe(dest('dist'));
@@ -21,11 +22,28 @@ const scss_task = () => src('app/scss/*.scss')
 .pipe(rename({suffix: '.min'}))
 .pipe(dest('dist/js'));
 
+function task_imgs() {
+    return src("app/img/*.+(jpg|jpeg|png|gif)")
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      interlaced: true
+    }))
+    .pipe(dest("dist/images"))
+  }
+  exports.imgs = task_imgs
+
+  const lib_css_task = () => src('app/lib/css/*.css')
+   .pipe(dest('dist/css'));
+
+   const lib_js_task = () => src('app/lib/js/*.js')
+   .pipe(dest('dist/js'));
+   
 const watch_task = () => {
     watch('app/*.html', parallel(html_task));
     watch('app/scss/*.scss', parallel(scss_task));
     watch('app/js/*.js', parallel(scripts_task));
+    watch('app/images/*.+(jpg|jpeg|png|gif', task_imgs);
 }
 
-exports.default=series(html_task, scss_task, scripts_task, watch_task);
- 
+exports.default=series(html_task, scss_task, scripts_task, task_imgs, lib_css_task, lib_js_task ,watch_task);
